4decd3e329a7a7b64d60cad9ae78a194e8fc4b7d
dom <dom@021ee608-1ed4-0310-a892-85bafff719e1>
2008-01-18 14:41:03 +0000

    Add in timing diagnostics.

--- src/main.rb
+++ src/main.rb
@@ -6,7 +6,12 @@ require 'solver'
 
 letters = %w[a b c d e f g h i j k l m n o p]
 
+puts ">> #{Time.now}: Making board."
 board = Board.new(letters)
+puts ">> #{Time.now}: Making dictionary."
 dict = Dictionary.new('/usr/share/dict/words')
+puts ">> #{Time.now}: Making Solver."
 solver = Solver.new(board, dict)
+puts ">> #{Time.now}: Solving."
 solver.all_words.each { |w| puts w }
+puts ">> #{Time.now}: Done."
