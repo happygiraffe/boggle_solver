d8f182d918828214cb7fd42029a5026214d1e7ea
dom <dom@021ee608-1ed4-0310-a892-85bafff719e1>
2008-01-20 09:19:21 +0000

    Take the letters from the command line rather than hard coding.

--- src/main.rb
+++ src/main.rb
@@ -4,7 +4,14 @@ require 'board'
 require 'dictionary'
 require 'solver'
 
-letters = %w[a b c d e f g h i j k l m n o p]
+def usage
+  $stderr.puts("usage: #{File.basename($0)} letters")
+  exit(2)
+end
+
+#letters = %w[a b c d e f g h i j k l m n o p]
+letters = ARGV.join(' ').split(/\s+/)
+usage unless letters.size == 16
 
 puts ">> #{Time.now}: Making board."
 board = Board.new(letters)
